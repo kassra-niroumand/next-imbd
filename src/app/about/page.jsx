2efa1a33879ff86c68ff139422079bcd45ac3597
kasra <kasra.niroumand@gmail.com>
2023-04-14 15:41:15 +0330

    about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className="text-2xl font-medium text-amber-600">
+            About
+        </h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis cupiditate dolorum neque ratione quas, sit voluptate autem ducimus doloribus soluta aliquid temporibus ipsum quis esse illo repudiandae obcaecati cumque perferendis dolor explicabo ab fugit, inventore suscipit. Labore vero aspernatur earum hic. Eveniet perspiciatis eaque accusantium quod doloribus dolorum ducimus porro?
+        </p>
+    </div>
+  )
+}
