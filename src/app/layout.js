16a4c5cfc09ba648a7b6d304ee394b0152c2dbd6
kasra <kasra.niroumand@gmail.com>
2023-04-14 16:18:07 +0330

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Providers from "./Providers";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
           {/* header */}
           <Header />
           {/* navbar */}
+          <Navbar />
           {/* searchBox */}
           {children}
         </Providers>
