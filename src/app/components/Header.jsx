f89401407602b4c3917aec7ac8585cbaa09ac19c
kasra <kasra.niroumand@gmail.com>
2023-04-14 15:34:01 +0330

    Add darkmode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
 
 
 export default function Header() {
@@ -10,9 +11,10 @@ export default function Header() {
     <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
         <div className="flex">
             <MenuItem title="Home" address="/" Icon={AiFillHome} />
-            <MenuItem title="About" address="/" Icon={BsFillInfoCircleFill} />
+            <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
         </div>
-        <div className="flex">
+        <div className="flex items-center space-x-5">
+            <DarkModeSwitch /> 
             <Link href="/">
                 <h2 className='text-2xl'>
                     <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMBD</span>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Providers from "./Providers";
 import Header from "./components/Header";
 import "./globals.css";
 
@@ -10,11 +11,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* header */}
-        <Header />
-        {/* navbar */}
-        {/* searchBox */}
-        {children}
+        <Providers>
+          {/* header */}
+          <Header />
+          {/* navbar */}
+          {/* searchBox */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
